f81448b466704022436e15bdae0a2e8a90a58763
alaister123 <alaistersun@hotmail.com>
2020-08-08 04:21:43 -0700

    add functions

--- Bot/YumiBot.py
+++ Bot/YumiBot.py
@@ -1,12 +1,14 @@
 import requests
+import time 
 import json
 
 class YumiBot:
     """Class for requesting data from Riot API """
     
-    def __init__(self, key, region = 'na1'):
+    def __init__(self, key, region = 'na1', rate = 50):
         self._api_key = key
         self._region = region
+        self._rate = rate
 
     def __str__(self):
         info = "Current Key: " + self._api_key + "\nCurrent Region: " + self._region
@@ -19,24 +21,41 @@ class YumiBot:
         self._region = region
 
     def request_by_summoner_name(self, name):
+        """ Get summoner's info from ign"""
         url = "https://" + self._region + ".api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key=" + self._api_key
         response = requests.get(url)
         return response.json()
 
     def get_matchlist_by_id(self, id):
-        url = "https://" + self._region + ".api.riotgames.com/lol/match/v4/matchlists/by-account/" + id + "?api_key=" + self._api_key
+        """ Get match list by account id"""
+        url = "https://" + self._region + ".api.riotgames.com/lol/match/v4/matchlists/by-account/" + id + "?queue=420&api_key=" + self._api_key
         response = requests.get(url)
         return response.json()
     
     def get_match_data(self,match_id):
+        """ Get match data based on match id """
         url = "https://" + self._region+ ".api.riotgames.com/lol/match/v4/matches/" + str(match_id) + "?api_key=" + self._api_key
         response = requests.get(url)
         return response.json()
 
+    def get_game_ids_by_name(self, name):
+        """
+            Returns 100 recent rank games for a player
+            name: name of the summoner
+            returns: list() game_id
+        """
+        id  = self.request_by_summoner_name(name)["accountId"]
+        data = self.get_matchlist_by_id(id)["matches"]
+        
+        game_id = list()
+        for index in data:
+            game_id.append(index["gameId"])
+        return game_id
+    
+
+        
 
 # testing
-a = YumiBot("abderf","na1")
-id = a.request_by_summoner_name("123")["accountId"]
-data = a.get_matchlist_by_id(id)
-l = data["matches"][0]["gameId"]
-print(a.get_match_data(l))
+a = YumiBot("mystery","na1")
+b = a.get_game_ids_by_name("asd")
+print(a.get_match_data(b[0])["teams"][0])
