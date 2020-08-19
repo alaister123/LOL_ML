633bb69284639e21924a096791f6d18f1693b2de
alaister123 <alaistersun@hotmail.com>
2020-08-18 17:36:42 -0700

    commented all func

--- Bot/YumiBot.py
+++ Bot/YumiBot.py
@@ -15,34 +15,91 @@ class YumiBot:
         return info
 
     def change_key(self, key):
+
         self._api_key = key
 
     def change_region(self, region):
+        """
+        Change region of the scraper 
+
+        Parameters
+        ----------
+        region : String
+            The region id defined by the riot api (defaulted na1)
+
+        Returns
+        -------
+        None
+        """
         self._region = region
 
     def request_by_summoner_name(self, name):
-        """ Get summoner's info from ign"""
+        """
+        Request player IDs by summoner name
+
+        Parameters
+        ----------
+        name : String
+            Player's ingame name
+
+        Returns
+        -------
+        response.json() : dict()
+            Json format of the result requested from Riot API
+        """
         url = "https://" + self._region + ".api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key=" + self._api_key
         response = requests.get(url)
         return response.json()
 
     def get_matchlist_by_id(self, id):
-        """ Get match list by account id"""
+        """
+        Get the most recent 100 game a player had played including special mode
+
+        Parameters
+        ----------
+        id : String
+            The id obtained in player information
+
+        Returns
+        -------
+        response.json() : dict()
+            Json format of the result requested from Riot API
+        """
         url = "https://" + self._region + ".api.riotgames.com/lol/match/v4/matchlists/by-account/" + id + "?queue=420&api_key=" + self._api_key
         response = requests.get(url)
         return response.json()
     
     def get_match_data(self,match_id):
-        """ Get match data based on match id """
+        """
+        Get specific information about a match by the match_id 
+
+        Parameters
+        ----------
+        match_id : int
+            The match id
+
+        Returns
+        -------
+        response.json() : dict()
+            Json format of the result requested from Riot API
+        """
         url = "https://" + self._region+ ".api.riotgames.com/lol/match/v4/matches/" + str(match_id) + "?api_key=" + self._api_key
         response = requests.get(url)
         return response.json()
 
     def get_game_ids_by_name(self, name):
         """
-            Returns 100 recent rank games for a player
-            name: name of the summoner
-            returns: list() game_id
+        Get and return the match_ids of a player's ranked games
+
+        Parameters
+        ----------
+        name : String
+            Player's ingame name
+
+        Returns
+        -------
+        game_id : List
+            A list of match ids of 100 ranked games 
         """
         id  = self.request_by_summoner_name(name)["accountId"]
         data = self.get_matchlist_by_id(id)["matches"]
@@ -52,8 +109,6 @@ class YumiBot:
             game_id.append(index["gameId"])
         return game_id
     
-
-        
 
 # testing
 a = YumiBot("mystery","na1")
